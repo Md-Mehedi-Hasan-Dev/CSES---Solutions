453f9ac29886a2cdfc83d89457cb79abe191cc07
Md. Mehedi Hasan <mehedi85879@gmail.com>
2026-07-11 10:40:11 +0600

    Basic CSES Practice

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,45 @@
+// - Md. Mehedi Hasan
+
+#include <bits/stdc++.h>
+#define Md_Mehedi_Hasan ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+#define nl '\n'
+#define sp " "
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<long long>
+#define ip(x) for (auto &it : x) cin >> it
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    ll n; cin >> n;
+    while(n != 1)
+    {
+        cout << n << sp;
+        (n & 1) ? n = (n * 3ll) + 1 : n /= 2ll;
+    }
+    cout << 1;
+    
+}
+
+int main()
+{
+    Md_Mehedi_Hasan;
+
+    solve();
+
+    return 0;
+}
+
+// https://cses.fi/problemset/task/1068
